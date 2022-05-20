f7855b4eb68a60fce703a5bb4b3dd2892397262e
dr0nn1 <peda_95@hotmail.com>
2022-05-21 00:11:09 +0200

    updated files

--- deepSORT/csvReader.py
+++ deepSORT/csvReader.py
@@ -1,4 +1,3 @@
-from cProfile import label
 import cv2
 import pandas as pd
 import ast
@@ -19,10 +18,6 @@ def reader(dir):
         data[i+1] = row
     return data
 
-
-def dataClean(data):
-    return data
-
 def splitTracjetoryInEmptyGraph(data,dir):
     walking = {}
     falling = {}
@@ -31,15 +26,14 @@ def splitTracjetoryInEmptyGraph(data,dir):
     for i in range(1,len(data)+1):
         if len(data[i]) >0:
             x,y = (data[i][-1])
-            if y > 160 and data[i][0][1] < y and x > 150 or data[i][0][1] > 200 and y > 200 and x > 150:
+            if y > 160 and data[i][0][1] < y and x > 150 or data[i][0][1] > 200 and y > 200 and x > 150 and data[i][0][1] < y:
                 falling[fIndex] = data[i]
                 fIndex +=1
-                print(i)
             else:
                 walking[wIndex] = data[i]
                 wIndex +=1
     tracjetoryInEmptyGraph(falling,dir,"Trajectories from fall")
-    tracjetoryInEmptyGraph(walking,dir,"Normal trajectories")
+    tracjetoryInEmptyGraph(walking,dir,"Non falling trajectories")
 
 def tracjetoryInEmptyGraph(data,dir,name):
   # initialize color map
@@ -77,7 +71,6 @@ def distancePlot(data,dir):
     
     val["x"] = distance
     sns.set_theme(style="whitegrid")
-    #sns.boxenplot(data=distance,orient="h")
     sns.boxenplot(x=val["x"]).set(xlabel='Distance')
     plt.grid(False)
     plt.title("Distance plot")
@@ -94,9 +87,9 @@ def trajectoryPlot(data,dir):
     val = [x,y]
     sns.set_theme(style="whitegrid")
     
-    sns.boxenplot(data=val)
+    sns.boxenplot(data=val).set(xlabel='<- X                Y ->')
     plt.grid(False)
-    plt.title("Tracjetory x and y")
+    plt.title("Number of times a coordinate is present")
     plt.savefig(f'{dir}/trajectoryPlot.png')
     plt.show()
     
@@ -128,8 +121,8 @@ def heatMap(data,dir):
     cmap = copy(plt.get_cmap('hot'))
     cmap.set_bad(cmap(0))
     plt.grid(False)
-    h = plt.hist2d(x,y, bins=[np.arange(0,640,2),np.arange(0,512,2)],cmap=cmap)
-    plt.title("heatmap")
+    h = plt.hist2d(x,y, bins=[np.arange(0,640,3),np.arange(0,512,3)],cmap=cmap)
+    plt.title("Heatmap")
     plt.gca().invert_yaxis()
     plt.grid(False)
     plt.colorbar(h[3])
@@ -156,12 +149,7 @@ def distanceMap(data,dir):
 
     cmap = copy(plt.get_cmap('hot'))
     cmap.set_bad(cmap(0))
-
-    #h = plt.hist2d(x,y,weights=distance,bins=[np.arange(0,640,5),np.arange(0,512,5)],cmap=cmap)
-    #plt.gca().invert_yaxis()
-    #plt.colorbar(h[3])
-    #plt.show()
-    plt.title("Distance")
+    plt.title("Distance between two points")
     plt.scatter(x, y, c=distance,cmap=cmap)
     plt.grid(False)
     plt.colorbar()
@@ -172,7 +160,7 @@ def distanceMap(data,dir):
     plt.show()
 
 
-def test(data,dir,name):
+def meanTrajcetory(data,dir,name):
   # initialize color map
     cmap = plt.get_cmap('tab20b')
     colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
@@ -194,34 +182,29 @@ def test(data,dir,name):
     endY = 0
     for i in range(1,len(data)+1):
         if len(data[i]) >0:
-            if (data[i][0][0]) < 250:                
+            if (data[i][0][0]) < 140:                
                 startX += (data[i][0][0])
                 startY += (data[i][0][1])
                 numberOfPointsStart +=1
 
-            elif (data[i][0][0]) > 400:                
+            elif (data[i][0][0]) > 300 and data[i][0][1] < 160:                
                 endX += (data[i][0][0])
                 endY += (data[i][0][1])
                 numberOfPointsEnd +=1
 
-            elif (data[i][-1][0]) > 400:
+            elif (data[i][-1][0]) > 300 and data[i][-1][1] < 160:
                 endX += (data[i][-1][0])
                 endY += (data[i][-1][1])
                 numberOfPointsEnd +=1
 
-            elif (data[i][-1][0]) < 250:                
+            elif (data[i][-1][0]) < 140:                
                 startX += (data[i][-1][0])
                 startY += (data[i][-1][1])
                 numberOfPointsStart +=1
 
-    #startPoint = (int(startX/numberOfPoints),int(startY/numberOfPoints))
-    #endPoint = (int(endX/numberOfPoints),int(endY/numberOfPoints))
-    #print(numberOfPointsStart,numberOfPointsEnd)
     startPoint = (0,int(startY/numberOfPointsStart))
     endPoint = (640,int(endY/numberOfPointsEnd))
     cv2.line(img,startPoint, endPoint,(0,255,0),2)
-
-
     plt.gca().invert_yaxis()
     plt.title(name)
     plt.xlabel("Pixel value")
@@ -231,54 +214,54 @@ def test(data,dir,name):
     plt.show()
     
 def oneDPlot(data,dir):
-    #60 good fall. 77 brukt som walk
+    for i in [9,69,72,78]:
     #for i in range(1,len(data)+1):
-    x =[]
-    y = []
-    #    if len(data[i])>5:
-    i= 169
-    for j in range(0, len(data[i])):
-        x.append(data[i][j][0])
-        y.append(data[i][j][1])
+        x =[]
+        y = []
+        if len(data[i])>10:
+            #i= 169
+            print(i)
+            for j in range(0, len(data[i])):
+                x.append(data[i][j][0])
+                y.append(data[i][j][1])
 
-    plt.plot(x,y)
-    plt.gca().invert_yaxis()
-    plt.title("1d plot walking")
-    plt.xlabel("x position")
-    plt.ylabel("y position")
-    plt.savefig(f'{dir}/1dPlotWalking2.png')
-    plt.show()
+            plt.plot(x,y)
+            plt.gca().invert_yaxis()
+            plt.title("1d plot non falling")
+            plt.xlabel("x position")
+            plt.ylabel("y position")
+            plt.savefig(f'{dir}/1dPlotWalking2.png')
+            plt.show()
 
-    time = []
-    for j in range(1, len(data[i])+1):
-        time.append(j)
+            time = []
+            for j in range(1, len(data[i])+1):
+                time.append(j)
 
-    plt.plot(time,x)
-    plt.plot(time,y)
-    plt.legend(["X","Y"])
-    plt.gca().invert_yaxis()
-    plt.title("Position versus frames (walking person)")
-    plt.xlabel("time (frame number)")
-    plt.ylabel("position")
-    plt.savefig(f'{dir}/xySplitWalking2.png')
-    plt.show()
+            plt.plot(time,x)
+            plt.plot(time,y)
+            plt.legend(["X","Y"])
+            plt.gca().invert_yaxis()
+            plt.title("Position versus frames (non falling persons)")
+            plt.xlabel("time (frame number)")
+            plt.ylabel("position")
+            plt.savefig(f'{dir}/xySplitWalking2.png')
+            plt.show()
 
-    img =  255 * np.ones(shape=[512, 640, 3], dtype=np.uint8)
-    for j in range(1, len(data[i])):
-        if data[i][j - 1] is None or data[i][j] is None:
-            continue
-        cv2.line(img,(data[i][j-1]), (data[i][j]),(0,0,255),1)
-    plt.gca().invert_yaxis()
-    plt.title("Walking trajcetory")
-    plt.xlabel("Pixel value")
-    plt.ylabel("Pixel value")
-    plt.imshow(img)
-    plt.savefig(f'{dir}/1dTrajcetoryWalking2.png')
-    plt.show()
+            img =  255 * np.ones(shape=[512, 640, 3], dtype=np.uint8)
+            for j in range(1, len(data[i])):
+                if data[i][j - 1] is None or data[i][j] is None:
+                    continue
+                cv2.line(img,(data[i][j-1]), (data[i][j]),(0,0,255),1)
+            plt.gca().invert_yaxis()
+            plt.title("Non falling trajcetory")
+            plt.xlabel("Pixel value")
+            plt.ylabel("Pixel value")
+            plt.imshow(img)
+            plt.savefig(f'{dir}/1dTrajcetoryWalking2.png')
+            plt.show()
 
 def comparePlot(data,dir):
-    #    if len(data[i])>5:
-    walk = [77,114,169]
+    walk = [9,69,72,78]
     for i in walk:
         x1 =[]
         y1 = []
@@ -290,10 +273,10 @@ def comparePlot(data,dir):
         for j in range(1, len(data[i])+1):
         
             time.append(j)
-        plt.plot(time,x1,'r--')
+        plt.plot(time,x1,'y--')
         plt.plot(time,y1,'r')
     
-    fall = [41,60,179]
+    fall =[16,39,52,65]
     for i in fall:
         x2 =[]
         y2 = []
@@ -304,33 +287,21 @@ def comparePlot(data,dir):
         time1 = []
         for j in range(1, len(data[i])+1):
             time1.append(j)
-        plt.plot(time1,x2,"b--")
+        plt.plot(time1,x2,"g--")
         plt.plot(time1,y2,"b")
 
     lines = plt.gca().get_lines()
-    legend2 = plt.legend([lines[i] for i in [0,1,6,7]],['Walk X','Walk Y','Fall X','Fall Y'], loc=4)
+    legend2 = plt.legend([lines[i] for i in [0,1,8,9]],['Walk X','Walk Y','Fall X','Fall Y'], loc=4)
     plt.gca().invert_yaxis()
-    plt.title("Position versus frames (falling and walking)")
+    plt.title("Position versus frames (falling and non falling)")
     plt.xlabel("time (frame number)")
     plt.ylabel("position")
     plt.savefig(f'{dir}/compare.png')
     plt.show()
 
-    
-
 if __name__ == '__main__':
-    #dir = 'C:/Users/peda_/Documents/Masteroppgave/deepSort/viz/grillstad'
-    #dir = 'C:/Users/peda_/Documents/Masteroppgave/deepSort/viz/new/solsiden/solsiden9'
-    #dir = 'C:/Users/peda_/Documents/Masteroppgave/deepSort/viz/new/solsiden/solsiden15'
-    #dir = 'C:/Users/peda_/Documents/Masteroppgave/deepSort/viz/new/solsiden/close'
-    #dir = 'C:/Users/peda_/Documents/Masteroppgave/deepSort/viz/new/solsiden/mid'
-    #dir = 'C:/Users/peda_/Documents/Masteroppgave/deepSort/viz/new/solsiden/far'
-    #dir = 'C:/Users/peda_/Documents/Masteroppgave/deepSort/viz/test'
-    dir = 'C:/Users/peda_/Documents/Masteroppgave/deepSort/viz/test/age20'
-
+    dir = 'C:/Users/peda_/Documents/Masteroppgave/deepSort/viz/test'
     data = reader(dir)
-    #print(data)
-    #data = dataClean(data)
     #trajectoryMap(data,dir)
     #trajectoryPlot(data,dir)
     #heatMap(data,dir)
@@ -338,9 +309,6 @@ if __name__ == '__main__':
     #distancePlot(data,dir)
     #tracjetoryInEmptyGraph(data,dir,"All trajectories")
     #splitTracjetoryInEmptyGraph(data,dir)
-    #test(data,dir,"test")
+    #meanTrajcetory(data,dir,"Mean trajcetory")
     #oneDPlot(data,dir)
     comparePlot(data,dir)
-
-
-    
